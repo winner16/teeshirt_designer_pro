2a9da3ddc842f5ea37b311e3a0152faf1cd16c15
winner16 <96954165+winner16@users.noreply.github.com>
2025-08-13 20:50:55 +0100

    Update Routes.jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,29 +3,29 @@ import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
 import ScrollToTop from "components/ScrollToTop";
 import ErrorBoundary from "components/ErrorBoundary";
 import NotFound from "pages/NotFound";
-import UserLogin from './pages/user-login';
-import DesignPreviewExport from './pages/design-preview-export';
-import MainDashboard from './pages/main-dashboard';
-import UserProfileSettings from './pages/user-profile-settings';
-import UserRegistration from './pages/user-registration';
-import DesignEditor from './pages/design-editor';
+
+import UserLogin from "pages/user-login";
+import DesignPreviewExport from "pages/design-preview-export";
+import MainDashboard from "pages/main-dashboard";
+import UserProfileSettings from "pages/user-profile-settings";
+import UserRegistration from "pages/user-registration";
+import DesignEditor from "pages/design-editor";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <ErrorBoundary>
-      <ScrollToTop />
-      <RouterRoutes>
-        {/* Define your route here */}
-        <Route path="/" element={<DesignEditor />} />
-        <Route path="/user-login" element={<UserLogin />} />
-        <Route path="/design-preview-export" element={<DesignPreviewExport />} />
-        <Route path="/main-dashboard" element={<MainDashboard />} />
-        <Route path="/user-profile-settings" element={<UserProfileSettings />} />
-        <Route path="/user-registration" element={<UserRegistration />} />
-        <Route path="/design-editor" element={<DesignEditor />} />
-        <Route path="*" element={<NotFound />} />
-      </RouterRoutes>
+        <ScrollToTop />
+        <RouterRoutes>
+          <Route path="/" element={<DesignEditor />} />
+          <Route path="/user-login" element={<UserLogin />} />
+          <Route path="/design-preview-export" element={<DesignPreviewExport />} />
+          <Route path="/main-dashboard" element={<MainDashboard />} />
+          <Route path="/user-profile-settings" element={<UserProfileSettings />} />
+          <Route path="/user-registration" element={<UserRegistration />} />
+          <Route path="/design-editor" element={<DesignEditor />} />
+          <Route path="*" element={<NotFound />} />
+        </RouterRoutes>
       </ErrorBoundary>
     </BrowserRouter>
   );
