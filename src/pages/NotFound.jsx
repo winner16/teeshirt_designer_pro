33683edde3bfefe951d744ce2f606f92f520e6c7
winner16 <96954165+winner16@users.noreply.github.com>
2025-08-14 22:17:16 +0100

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import Button from 'components/ui/Button';
-import Icon from 'components/AppIcon';
+import Button from '../components/ui/Button';
+import Icon from '../components/AppIcon';
 
 const NotFound = () => {
   const navigate = useNavigate();
